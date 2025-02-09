eaead4bbad2b953a59aae059750ecec5f0622c8d
primejosamen <ing.josemendez11@gmail.com>
2025-02-09 01:49:42 -0500

    ZXZ
    zXZ

--- terraform/create_env.sh
+++ terraform/create_env.sh
@@ -11,18 +11,18 @@ fi
 
 # Define the environment variable content
 env_content=$(cat <<EOF
-CCLOUD_API_KEY=api-key
-CCLOUD_API_SECRET=api-secret
-CCLOUD_BOOTSTRAP_ENDPOINT=kafka-cluster-endpoint
+CCLOUD_API_KEY=NELWGHSLGNWSKAKH
+CCLOUD_API_SECRET=cZc6ljuwy+pZ3V6ZkArR7KVNF8cD4HFIIiIIq+iMUZduF3n9n5q90SRS9j/qsY7W
+CCLOUD_BOOTSTRAP_ENDPOINT=https://psrc-8kz20.us-east-2.aws.confluent.cloud
 
 CCLOUD_SCHEMA_REGISTRY_API_KEY=sr-key
 CCLOUD_SCHEMA_REGISTRY_API_SECRET=sr-secret
 CCLOUD_SCHEMA_REGISTRY_URL=sr-cluster-endpoint
 
 MONGO_USERNAME=admin
-MONGO_PASSWORD=db-sd-c0nflu3nt!
+MONGO_PASSWORD=password123
 MONGO_ENDPOINT=mongodb-endpoint
-MONGO_DATABASE_NAME=demo-stream-designer
+MONGO_DATABASE_NAME=mongodb-e5008c4210923563e43221c100866090cdffb5c95e7c97709703708f3d121232
 EOF
 )
 
